a564a2435dc1dc1f966dea30f4542dea068efb8d
stu_bird <2906997669@qq.com>
2023-04-11 09:18:59 +0800

    修改vhdlEditor组件使vhdl代码高亮

--- src/components/vhdlEditor/index.vue
+++ src/components/vhdlEditor/index.vue
@@ -1,31 +1,52 @@
 <template>
   <div>
-    <codemirror :v-model="vhdlCode" :options="editorOptions" >
-    </codemirror>
+    <Codemirror
+      ref="cm"
+      :options="cmOptions"
+      :value = "code" 
+      @input="inputChange"
+    >
+    </Codemirror>
   </div>
 </template>
 
 <script>
-import {Codemirror} from "vue-codemirror";
-
-// import 'codemirror/lib/codemirror.css';
-// import 'codemirror/mode/vhdl/vhdl'
-// import 'codemirror/theme/eclipse.css'
-// import 'vue-codemirror/dist/vue-codemirror.css'
+  import { ref, onMounted, onUnmounted } from "vue"
+  import "codemirror/mode/vhdl/vhdl.js"
+  import "codemirror/mode/javascript/javascript.js"
+  import 'codemirror/theme/material.css';
+  import Codemirror from "codemirror-editor-vue3"
 
 export default {
   name: "vhdlEditor",
   components: {Codemirror},
-  data(){
-    return{
-      vhdlCode:"",
-      editorOptions: {
-        mode: 'vhdl',
-        lineNumber: true,
-        theme: 'eclipse',
+  data() {
+      return{
+        code: 'LIBRARY ieee;',
+        cmOptions: {
+          // 语言及语法模式
+          mode: 'text/x-vhdl',
+          // 主题
+          
+          // 显示函数
+          line: true,
+          lineNumbers: true,
+          // 软换行
+          lineWrapping: true,
+          // tab宽度
+          tabSize: 4,
+        }
       }
+    },
+    methods: {
+      inputChange(content) {
+        this.code = content;
+        this.$nextTick(() => {
+          console.log("code:" + this.code);
+          console.log("content:" + content)
+        });
+      },
     }
-  }
 }
 </script>
 
